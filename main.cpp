17a805177c6938303178c4a7d8f5d241c4dcb614
sergeyiandronov <sergeyiandronov@gmail.com>
2017-11-20 16:30:29 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <sstream>
+using namespace std;
+void merge(int *&tmp,int *leftpart,int *rightpart,int sizeint sizel,int sizer){
+    int lp=0,rp=0,n=0;
+	while(n<size){
+		if(leftpart[lp]<=rightpart[rp]){
+			tmp[n]=leftpart[lp];
+			lp++;
+		}
+		if(leftpart[lp]>rightpart[rp]){
+			tmp[n]=rightpart[rp];
+			rp++;
+		}
+	}
+}
+void merge_sort(int *&ar,int size){
+ 	
+}
+int main() {
+   unsigned  int n;
+   char e;
+    string line;
+    getline(cin,line);
+    istringstream numstream(line);
+    if(!(numstream>>n)||!(n>0)){
+        cout<<"An error has occuried while reading input data.";
+        exit(0);
+    }
+
+    getline(cin,line);
+    istringstream arrstream(line);
+    int *arr=new int[n];
+    for(int i=0;i<n;i++){
+        if(!(arrstream>>arr[i])){
+            cout<<"An error has occuried while reading input data.";
+            delete[] arr;
+            exit(0);
+        }
+
+    }
+    if(arrstream>>e){
+         cout<<"An error has occuried while reading input data.";
+            delete[] arr;
+            exit(0);	
+    }
+
+    
+    
+   quicksort(arr,0,n-1,arr[0]);
+   
+
+    
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    delete[] arr;
+    return 0;
+}
